fca7b7a2f8b47d14d0d1e2ef05cfec0c928df371
rkeppke <realkeppke@gmail.com>
2023-01-21 18:43:27 -0400

    added a easter egg that shows up after you try to press "create box" too many times

--- trick1/js-appear-dissapear.js
+++ trick1/js-appear-dissapear.js
@@ -1,21 +1,44 @@
+let clickCount = 0;
+
+//makes sure page is loaded first to avoid problems
 window.addEventListener("load", function(){
 
+    //selects burron1 element and waits for the click event
     document.querySelector(".button1").addEventListener("click", function(){
-        let background = document.createElement("div"); //creates new bg element
-        background.classList.add("background"); //creates class for element
-        document.body.appendChild(background); //appends 
 
-        let disappearButton = document.createElement("button");
+        clickCount++; //updates how many times create button was pressed
+
+        if (clickCount > 3)
+        {
+            //breaking point
+            let text = document.createElement("div");
+            text.innerHTML = "3 times is enough";
+            text.classList.add("text");
+            document.body.appendChild(text);
+            this.style.display = "none"; // hides button
+        }
+        else {
+        //creates new background
+        let background = document.createElement("div"); //created div
+        background.classList.add("background"); //added "background" class
+        document.body.appendChild(background); //appends to body element
+
+        //creates new button
+        let disappearButton = document.createElement("button"); 
         disappearButton.classList.add("disappear-button");
-        disappearButton.innerHTML = "make this box dissapear";
+        disappearButton.innerHTML = "make this box dissapear"; //creates button txt
         background.appendChild(disappearButton);
 
         this.style.display = "none"; // hiding the original button
 
-        disappearButton.addEventListener("click", function(){
-        background.remove();
-        this.remove();
-        document.querySelector(".button1").style.display = "block";
-        });
+        removeBox = function() {
+            background.remove();
+            disappearButton.remove();
+            document.querySelector(".button1").style.display = "block";
+          }
+        
+        disappearButton.addEventListener("click", removeBox);
+        }
     });
 });
+
